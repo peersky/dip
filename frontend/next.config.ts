300fdae563c5c8cb6140aeed0172225822ecf61c
Tim Pechersky <tim.pechersky@gmail.com>
2025-08-12 15:56:53 +0700

    test

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -21,13 +21,11 @@ export default {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   reactStrictMode: false,
   trailingSlash: false,
+  output: "standalone",
   // output: "standalone",
   // target: "serverless",
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  webpack: (
-    config: any,
-    { isServer, webpack }: { isServer: boolean; webpack: any },
-  ) => {
+  webpack: (config: any, { isServer, webpack }: { isServer: boolean; webpack: any }) => {
     const envs: { [key: string]: string } = {};
     Object.keys(process.env).forEach((env) => {
       if (env.startsWith("NEXT_PUBLIC_")) {
@@ -40,7 +38,7 @@ export default {
         new webpack.DefinePlugin({
           "process.env": JSON.stringify(envs),
           "import.meta.webpackHot": "module.hot",
-        }),
+        })
       );
     }
 
@@ -63,7 +61,7 @@ export default {
     config.plugins.push(
       new webpack.IgnorePlugin({
         resourceRegExp: /^electron$/,
-      }),
+      })
     );
 
     if (config.cache && !isServer) {
@@ -80,6 +78,7 @@ export default {
     ignoreDuringBuilds: true,
   },
   experimental: {
+    externalDir: true,
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
   transpilePackages: ["@peeramid-labs/dip-core", "@peeramid-labs/dip-database"],
